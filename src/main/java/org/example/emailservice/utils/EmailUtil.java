5711ef61a78fa41ace995ad9c323c921b92bfcc0
Sagar Pandey <sagar@Sagars-MacBook-Air.local>
2026-01-25 19:39:46 +0530

    Add project source files

---
+++ src/main/java/org/example/emailservice/utils/EmailUtil.java
@@ -0,0 +1,30 @@
+package org.example.emailservice.utils;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Component
+public class EmailUtil {
+
+    private final JavaMailSender mailSender;
+
+    public EmailUtil(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String to, String from, String subject, String body) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setFrom(from);
+        helper.setSubject(subject);
+        helper.setText(body, true); // true indicates HTML
+
+        mailSender.send(message);
+    }
+}
